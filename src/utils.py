561e724161a6da68f09dbd2078029ae97721b367
christopher-w-murphy <murphtron5000@gmail.com>
2019-10-26 22:33:21 -0400

    updated analysis

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import pickle
 
-def pkl_save_obj(obj, name):
-    with open('results/' + name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+def pkl_save_obj(obj, file_path):
+    with open(file_path, 'wb') as f:
+        if file_path.endswith('pkl'):
+            pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-def pkl_load_obj(name):
-    with open('results/' + name + '.pkl', 'rb') as f:
+def pkl_load_obj(file_path):
+    with open(file_path, 'rb') as f:
         return pickle.load(f)
