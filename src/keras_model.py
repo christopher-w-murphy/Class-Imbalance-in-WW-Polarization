a0d469d3de9bfb3eb5dd8a9ae556391f50545124
christopher-w-murphy <murphtron5000@gmail.com>
2019-11-23 17:17:28 -0500

    ensured focal loss is consistent w/ tf2

--- src/keras_model.py
+++ src/keras_model.py
@@ -1,31 +1,21 @@
 from tensorflow.keras.layers import Activation, BatchNormalization, Dense, Dropout
 from tensorflow.keras.models import Sequential
 
-def build_model(n_features, n_hidden_layers=2, n_neurons=150, loss_function='binary_crossentropy'):
-    """
-    deep, fully-connected neural network in Keras w/ Tensorflow backend
-    outputs a binary classification probability
-    input and hidden layers have 150 nodes (by default)
-    are initialized w/ He
-    have relu activation functions
-    and are regularized using dropout w/ a 50% rate and batch normalization
-    output layer has a sigmoid activation function
-    the adam optimizer is used
-    """
+def build_model(n_features, n_hidden_layers=2, n_neurons=150, loss_function='binary_crossentropy', final_bias='zeros'):
     model = Sequential()
 
-    model.add(Dense(n_neurons, input_dim=n_features, kernel_initializer='he_normal'))
+    model.add(Dense(n_neurons, input_dim=n_features))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
 
     for _ in range(1, n_hidden_layers):
-        model.add(Dense(n_neurons, kernel_initializer='he_normal'))
+        model.add(Dense(n_neurons))
         model.add(BatchNormalization())
         model.add(Activation('relu'))
         model.add(Dropout(0.5))
 
-    model.add(Dense(1))
+    model.add(Dense(1, bias_initializer=final_bias))
     model.add(BatchNormalization())
     model.add(Activation('sigmoid'))
 
